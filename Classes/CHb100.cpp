6af0e59e79309b54573284c567356be491771cec
FrSalgado <flsalgado.2000@gmail.com>
2023-01-19 19:34:05 +0000

    Created

---
+++ Classes/CHb100.cpp
@@ -0,0 +1,27 @@
+#include "CHb100.h"
+
+CHb100::CHb100(void)
+{
+    int32_t number;
+    struct sigaction act;
+
+    printf("\nOpening Driver\n");
+    fd = open("/dev/hb100_device", O_RDWR);
+    if(fd < 0) {
+            printf("Cannot open device file...\n");
+    }
+ 
+    printf("Registering application ...");
+    /* register this task with kernel for signal */
+    if (ioctl(fd, REG_CURRENT_TASK,(int32_t*) &number)) {
+        printf("Failed\n");
+        close(fd);
+        exit(1);
+    }
+    printf("Done!!!\n");
+}
+
+CHb100::~CHb100(void)
+{
+    close(fd);
+}
---
+++ Classes/CHb100.h
@@ -0,0 +1,34 @@
+#ifndef __CHB100_H_
+#define __CHB100_H_
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+
+#define SIGETX 44
+
+#define REG_CURRENT_TASK _IOW('a','a',int32_t*)
+
+class CHb100
+{
+    private:
+
+        int fd;
+
+    public:
+
+        CHb100(void);
+
+        ~CHb100(void);
+
+        void sig_event_handler(int, siginfo_t *, void *);
+};
+
+
+#endif
